7aa392e3ac9860d996b7c45781dc861fd010f5a6
JesusNeyr <jesua.park@gmail.com>
2022-12-06 16:56:44 -0300

    agrengando el mapa a la vista map

--- js/eventos_mapa.js
+++ js/eventos_mapa.js
@@ -1,44 +1,50 @@
 window.addEventListener("load",()=>{
-    const ul_de_map=document.querySelector("ul")
-    const li=document.createElement("li")
-    const a=document.createElement("a")
-    const login_li=document.querySelector('[name=login_li]')
-    const contenido=document.createTextNode("log out")
-    login_li.style.display="none"
-    a.appendChild(contenido)
-    li.appendChild(a)
-    li.style.cursor="pointer"
-    ul_de_map.appendChild(li)
+
+    mostrar=true
+    clave=''
     Object.entries(localStorage).forEach(([key, valor]) => {
-                if (key!=="login" && valor!=="true") {
-                    a.addEventListener("click",()=>{
-                        a.setAttribute("href","/view/home.html")
-                        localStorage.removeItem(key)
-                    })
-                }        
-                else{
-                    document.querySelector('body').style.display="none"
-                    document.querySelector('html').innerHTML='<h2>Error 404 page not found</h2>'
-                }
-            });
+        console.log(key,valor)
+        if (key!=="login" && valor!=="true") {
+            
+            mostrar=false
+        }else{
+            mostrar=true
+            clave=key
+        }
+    });             
+    if (mostrar) {
+
+        console.log("no debe estar log out , si log out 1");
+    }else{
+        
+        window.location.href="../view/login.html"
+    }
+    console.log(mostrar);
+// ------------------------------
+
+// En center guardamos las coordenadas donde se centra el mapa
+let center = [25.2841478, 51.4419568];
+
+// En la variable map enviamos el centro del mapa y el zoom inicial (12).
+let map = L.map("map").setView(center, 12);
+
+// Creamos el mapa
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(map);
+
+// ícono de Messi
+let messiIcon = L.icon({
+  iconUrl: "messi.png",
+  iconSize: [60, 60],
+  iconAnchor: [22, 44],
+  popupAnchor: [8, -41],
+});
+
+// Ponemos el ícono en el mapa.
+let marker = L.marker(center, { icon: messiIcon }).addTo(map);
+
+// Agregamos el pop up.
+marker.bindPopup("<b>Messi</b><br>Los amo!").openPopup();
+
 })
-// const ul_de_map=document.querySelector("ul")
-// const li=document.createElement("li")
-// const a=document.createElement("a")
-// const login_li=document.querySelector('[name=login_li]')
-// const contenido=document.createTextNode("log out")
-// login_li.style.display="none"
-// a.appendChild(contenido)
-// li.appendChild(a)
-// li.style.cursor="pointer"
-// ul_de_map.appendChild(li)
-// console.log(localStorage)
-// a.addEventListener("click",()=>{
-//     a.setAttribute("href","/view/home.html")
-//     Object.entries(localStorage).forEach(([key, valor]) => {
-//         if (key==="login" && valor==="true") {
-//             localStorage.removeItem(key)
-//         }        
-//     });
-    
-// })
--- js/eventos_nav.js
+++ js/eventos_nav.js
@@ -2,7 +2,54 @@
 const burguer=document.querySelector('#burger')
 const ul=document.querySelector("ul")
 const formulario_general=document.querySelector('.form_general')
+
+const li=document.createElement("li")
+const a=document.createElement("a")
+
+const li_de_map=document.createElement("li")
+const a_de_map=document.createElement("a")
+const contenido_de_a_map=document.createTextNode("Map")
+
+const login_li=document.querySelector('[name=login_li]')
+const contenido=document.createTextNode("Log out")
+
 burguer.addEventListener("click",()=>{
     ul.classList.toggle("show")
     formulario_general.classList.toggle("form_general")
 })
+mostrar=true
+Object.entries(localStorage).forEach(([key, valor]) => {
+    console.log(key,valor)
+    if (key!=="login" && valor!=="true") {
+        
+        mostrar=false
+    }else{
+        mostrar=true
+        clave=key
+    }
+});             
+if (mostrar) {
+    login_li.style.display="none"
+    a_de_map.appendChild(contenido_de_a_map)
+    li_de_map.appendChild(a_de_map)
+    li_de_map.style.cursor="pointer"
+    ul.appendChild(li_de_map)
+
+    a.appendChild(contenido)
+    li.appendChild(a)
+    li.style.cursor="pointer"
+    ul.appendChild(li)
+
+    a.addEventListener("click",()=>{
+        a.setAttribute("href","/view/home.html")
+        localStorage.removeItem(clave)
+    })
+    a_de_map.addEventListener("click",()=>{
+        a_de_map.setAttribute("href","/view/map.html")
+    })
+    console.log("hola");
+}
+// else{
+//     document.querySelector('body').style.display="none"
+//     document.querySelector('html').innerHTML='<h2>Error 404 page not found</h2>'
+// }
